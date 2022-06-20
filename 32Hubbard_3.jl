21373ed6feaaf0c29e1b2a22bc0f072eeb04fc21
denisthenichita <107113106+denisthenichita@users.noreply.github.com>
2022-06-20 15:43:50 +0300

    Update 32Hubbard_3.jl

--- 32Hubbard_3.jl
+++ 32Hubbard_3.jl
@@ -127,21 +127,21 @@ let
   avgT = inner(psi',T,psi)/N_phys
   println("\n<T3/2> = $avgT")
 
-  ################## p = 1/L sum alpha*<n>
+  ################## p*L = sum alpha*<n>
   ampo = OpSum()
 
   for b_phys in 1:N_phys
     b=2*b_phys-1
-    ampo += -1/2/N, "Nup", b  
-    ampo += -3/2/N, "Nup", b+1
-    ampo += 1/2/N, "Ndn", b  
-    ampo += 3/2/N, "Ndn", b+1    
+    ampo += -1/2, "Nup", b  
+    ampo += -3/2, "Nup", b+1
+    ampo += 1/2, "Ndn", b  
+    ampo += 3/2, "Ndn", b+1    
   end
 
-  p=MPO(ampo,sites)
+  pL=MPO(ampo,sites)
 
-  avgq = inner(psi',p,psi)/N_phys
-  println("\np = $avgp")
+  avgpL = inner(psi',pL,psi)/N_phys
+  println("\n p = $avgpL")
 
 
 end
